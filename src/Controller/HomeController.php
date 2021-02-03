139a5007e77b64ca6c3c9c59a242fa56c51f7940
bloic <loic.benos@gmail.com>
2021-02-03 13:29:57 +0100

    picture admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PictureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,13 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param PictureRepository $pictureRepository
+     * @return Response
      */
-    public function index(): Response
+    public function index(PictureRepository $pictureRepository): Response
     {
         return $this->render('home/index.html.twig', [
+            'pictures'=>$pictureRepository->findAll(),
         ]);
     }
 }
